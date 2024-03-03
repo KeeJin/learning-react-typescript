3a6c3187216931502bd12d3cfad2747867ee13df
keejin <yoplocheo@gmail.com>
2024-03-03 22:01:30 +0800

    minor cleanup

--- test-project/src/components/VideoViewport.tsx
+++ test-project/src/components/VideoViewport.tsx
@@ -11,7 +11,6 @@ interface VideoViewportProps {
 
 const VideoViewport: React.FC<VideoViewportProps> = ({ src, muted = true, loop = true, width = 640, height = 360 }) => {
     console.info("viewport rendered, ", src);
-    const [isPlaying, setIsPlaying] = useState(false);
     const [isHovered, setIsHovered] = useState(false);
 
     useEffect(() => {
@@ -20,10 +19,8 @@ const VideoViewport: React.FC<VideoViewportProps> = ({ src, muted = true, loop =
         const handleInViewChange = () => {
             if (video && video.paused) {
                 video.play();
-                setIsPlaying(true);
             } else if (video && !video.paused) {
                 video.pause();
-                setIsPlaying(false);
             }
         };
 
@@ -38,17 +35,17 @@ const VideoViewport: React.FC<VideoViewportProps> = ({ src, muted = true, loop =
         <div className="container mx-auto p-4 flex justify-center">
             <div className="relative" style={{ width: 640, height: 480 }} onMouseEnter={() => setIsHovered(true)}
                 onMouseLeave={() => setIsHovered(false)}>
-            <video
-                id="video"
-                src={src}
-                autoPlay={false}
-                muted={muted}
-                loop={loop}
-                width={width}
-                height={height}
-                controls={isHovered}
-                className=" object-cover"
-            />
+                <video
+                    id="video"
+                    src={src}
+                    autoPlay={false}
+                    muted={muted}
+                    loop={loop}
+                    width={width}
+                    height={height}
+                    controls={isHovered}
+                    className=" object-cover"
+                />
             </div>
         </div>
     );
